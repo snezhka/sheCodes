e35f469b47baf83caae2b616d90fbcbbdd2dd7d5
snezhka <ekaterina.latsko@gmail.com>
2023-01-08 16:17:26 +0100

    final fixes

--- src/index.js
+++ src/index.js
@@ -47,7 +47,6 @@ function showDefaultCityTemperature() {
 	city.innerHTML = `${cityDefault}`;
 	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityDefault}&key=${apiKey}&units=${units}`;
 	axios.get(apiUrl).then(function (response) {
-		console.log(response);
 		celciusTemp = response.data.temperature.current;
 		let temperature = Math.round(celciusTemp);
 		let desc = document.querySelector("#desc");
@@ -63,6 +62,7 @@ function showDefaultCityTemperature() {
 		wind.innerHTML = Math.round(response.data.wind.speed);
 		icon.setAttribute("src", response.data.condition.icon_url);
 	});
+	getForecast(cityDefault);
 }
 
 function searchWeather() {
@@ -80,7 +80,6 @@ function searchWeather() {
 		axios.get(apiUrl).then(function (response) {
 			celciusTemp = response.data.temperature.current;
 			let temperature = Math.round(celciusTemp);
-			console.log(response);
 			let city = document.querySelector("#city");
 			let cityVal = response.data.city;
 			let temp = document.querySelector("#temp");
@@ -97,6 +96,7 @@ function searchWeather() {
 			wind.innerHTML = Math.round(response.data.wind.speed);
 			icon.setAttribute("src", response.data.condition.icon_url);
 		});
+		getForecast(search.value);
 	}
 }
 
@@ -122,6 +122,45 @@ function showCelcius() {
 		temp.innerHTML = Math.round(celciusTemp);
 		far.classList.remove("active");
 		cel.classList.add("active");
+	});
+}
+
+function addForecastHTML(response) {
+	let forecast = document.querySelector(".forecast");
+	let days = response.data.daily;
+	let forecastHTML = "";
+	days.forEach(function (day, index) {
+		if (index < 5) {
+			forecastHTML =
+				forecastHTML +
+				`
+				<div>
+					<p class="mb-0">${convertToDate(day.time)}</p>
+					<img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+						day.condition.icon
+					}.png" alt="sunny weather" class="icon-forecast" />
+					<p style="font-size:15px;">${Math.round(
+						day.temperature.maximum
+					)} °C / ${Math.round(day.temperature.minimum)} °C</p>
+				</div>`;
+		}
+	});
+	forecast.innerHTML = forecastHTML;
+}
+
+function convertToDate(time) {
+	let ms = time * 1000;
+	let dateObj = new Date(ms);
+	let day = dateObj.getDay();
+	let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+	return days[day];
+}
+
+function getForecast(city) {
+	let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
+	axios.get(apiUrl).then(function (response) {
+		console.log(response);
+		addForecastHTML(response);
 	});
 }
 
